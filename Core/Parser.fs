9c5e97d2065e0b42949e62258d1d233ce0d89a7e
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-02-14 12:38:46 +0000

    front: Add an alternative parser using Farkle

--- Core/Parser.fs
+++ Core/Parser.fs
@@ -5,6 +5,7 @@ open System.IO
 open FSharp.Text.Lexing
 open FSharp.Text.Parsing
 open Fang.Lang
+open Farkle.Builder.OperatorPrecedence
 
 type token = GeneratedParser.token
 
@@ -72,3 +73,144 @@ let tryParse s : Option<Expr> =
     | _, errors -> raise (ParseException errors)
 
 let parse (s: string) : Expr = Option.get (tryParse s)
+
+module V2 =
+    open Farkle
+    open Farkle.Builder
+
+    let handleNeg expr =
+        match expr with
+        | Lit (BType.Int i) -> Lit(BType.Int(-i))
+        | _ -> Builtin(UnaryArithmetic(Neg, expr))
+
+    let rec mkApp expr args =
+        match args with
+        | [ only ] -> App(expr, only)
+        | h :: t -> mkApp (App(expr, h)) t
+        | [] -> failwith "empty arg list"
+
+    let rec mkBind isRec name args body expr =
+        let core = body
+
+        let unsugaredBody =
+            List.foldBack (fun n acc -> Abs(n, acc)) args core
+
+        Bind(isRec, name, unsugaredBody, expr)
+
+    let rec mkAbs names body =
+        List.foldBack (fun n acc -> Abs(n, acc)) names body
+
+    let intNum =
+        Terminals.unsignedIntRegex
+        |> terminal "int" (T(fun _ s -> int (s.ToString())))
+
+    let ident =
+        Regex.regexString @"[_a-zA-Z][_a-zA-Z0-9']*"
+        |> terminal "ident" (T(fun _ s -> VarName.make (s.ToString())))
+
+    let idents = many1 ident
+
+    let expr =
+        let P_APP = obj ()
+        let P_NEG = obj ()
+        let P_SIMPLE = obj ()
+
+        let simpleExpr: Nonterminal<Expr> = nonterminal "simple expr"
+        let simpleExprs = many1 simpleExpr
+
+        let expr: Nonterminal<Expr> = nonterminal "expr"
+
+        let absExpr =
+            "abstraction"
+            ||= [ !& @"\" .>>. idents .>> "." .>>. expr => mkAbs ]
+
+        let condExpr =
+            "cond"
+            ||= [ !& "if" .>>. expr .>> "then" .>>. expr .>> "else"
+                  .>>. expr
+                  => (fun pred t f -> Cond(pred, t, f)) ]
+
+        let letExpr =
+            "let"
+            ||= [ !& "let" .>>. idents .>> "=" .>>. expr .>> "in"
+                  .>>. expr
+                  => (fun ids body -> mkBind false (List.head ids) (List.tail ids) body)
+                  !& "let" .>> "rec" .>>. idents .>> "=" .>>. expr
+                  .>> "in"
+                  .>>. expr
+                  => (fun ids body -> mkBind true (List.head ids) (List.tail ids) body) ]
+
+        let appExpr =
+            "application"
+            ||= [ !@simpleExpr .>>. simpleExprs |> prec P_APP
+                  => mkApp ]
+
+        let prefixExpr =
+            "prefix expr"
+            ||= [ !& "-" .>>. simpleExpr |> prec P_NEG => handleNeg ]
+
+        let infixExpr =
+            "infix expr"
+            ||= [ !@expr .>> "+" .>>. expr
+                  => (fun l r -> Builtin(Arithmetic(ArithmeticFn.Add, l, r)))
+                  !@expr .>> "-" .>>. expr
+                  => (fun l r -> Builtin(Arithmetic(ArithmeticFn.Sub, l, r)))
+                  !@expr .>> "*" .>>. expr
+                  => (fun l r -> Builtin(Arithmetic(ArithmeticFn.Mul, l, r)))
+                  !@expr .>> "/" .>>. expr
+                  => (fun l r -> Builtin(Arithmetic(ArithmeticFn.Div, l, r)))
+
+                  !@expr .>> "=" .>>. expr
+                  => (fun l r -> Builtin(Comparison(ComparisonFn.Equal, l, r)))
+                  !@expr .>> "<" .>>. expr
+                  => (fun l r -> Builtin(Comparison(ComparisonFn.Less, l, r)))
+                  !@expr .>> ">" .>>. expr
+                  => (fun l r -> Builtin(Comparison(ComparisonFn.Greater, l, r))) ]
+
+        simpleExpr.SetProductions(
+            !@intNum => (fun i -> Lit(BType.Int i)), //
+            !@ident => Var,
+            !& "(" .>>. expr .>> ")" |> asIs
+        )
+
+        expr.SetProductions(
+            !@simpleExpr |> asIs,
+            !@absExpr |> asIs,
+            !@condExpr |> asIs,
+            !@letExpr |> asIs,
+            !@appExpr |> asIs,
+            !@prefixExpr |> asIs,
+            !@infixExpr |> asIs
+        )
+
+        let opScope =
+            OperatorScope(
+                LeftAssociative("if", "then", "else", "let", "in"),
+                RightAssociative(@"\", "."),
+                LeftAssociative("=", "<", ">"),
+                LeftAssociative("+", "-"),
+                LeftAssociative("*", "/"),
+                LeftAssociative(P_APP),
+                PrecedenceOnly(P_NEG)
+            )
+
+        DesigntimeFarkle.withOperatorScope opScope expr
+
+    let prog = opt expr
+
+    exception ParseException of msg: string with
+        override this.Message = this.msg
+
+    let parser = RuntimeFarkle.build prog
+
+    let tryParse (s: string) : Option<Expr> =
+        match RuntimeFarkle.parseString parser s with
+        | Ok expr -> expr
+        | Error err -> raise (ParseException(err.ToString()))
+
+    let parse (s: string) : Expr = Option.get (tryParse s)
+
+    let tryParseWithDiag (s: string) : Option<Expr> * List<string> =
+        match RuntimeFarkle.parseString parser s with
+        | Ok expr -> expr, []
+        | Error err -> None, [ err.ToString() ]
--- Tests/ParserTests.fs
+++ Tests/ParserTests.fs
@@ -1,5 +1,6 @@
 module Fang.Tests
 
+open System
 open Snapper
 open Snapper.Attributes
 open Xunit
@@ -33,7 +34,7 @@ module ParserTests =
     let doubleNeg () =
         let _, errors = tryParseWithDiag "--6"
         errors.ShouldMatchSnapshot()
-        
+
     [<Fact>]
     let plusMulMixed () =
         (tryParse "2 + 3 * 4").ShouldMatchSnapshot()
@@ -60,15 +61,16 @@ let y = 6"
     let multiArgAbs () =
         (tryParse @"\a b c. b*b - 4*a*c")
             .ShouldMatchSnapshot()
-            
+
     [<Fact>]
     let letInArithmetic () =
         let source = @"let inc x = x + 1 in inc 3"
         (tryParse source).ShouldMatchSnapshot()
-        
+
     [<Fact>]
     let digitsInIdents () =
-        (tryParse "let x2 = 42 in x2").ShouldMatchSnapshot()
+        (tryParse "let x2 = 42 in x2")
+            .ShouldMatchSnapshot()
 
     [<Fact>]
     let fib () =
@@ -76,3 +78,79 @@ let y = 6"
             tryParse (@"let rec fib n = if n < 2 then 1 else fib (n-1) + fib (n-2) in fib 10")
 
         ast.ShouldMatchSnapshot()
+
+[<StoreSnapshotsPerClass>]
+module ParserV2Tests =
+    open Parser.V2
+
+    let checkParseSnapshot src =
+        match tryParseWithDiag src with
+        | Some expr, [] ->
+            let repr = expr.ToString()
+
+            let lines =
+                repr.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+
+            lines.ShouldMatchSnapshot()
+        | _, errors -> errors.ShouldMatchSnapshot()
+
+    [<Fact>]
+    let ``nop`` () =
+        let r = tryParse ""
+        Assert.Equal(r, None)
+
+    [<Fact>]
+    let num () = checkParseSnapshot "1"
+
+    [<Fact>]
+    let negNum () = checkParseSnapshot "-1"
+
+    [<Fact>]
+    let opAdd () = checkParseSnapshot "1+2"
+
+    [<Fact>]
+    let opSub () = checkParseSnapshot "1-5"
+
+    [<Fact>]
+    let subComplex () = checkParseSnapshot "-(42-17)"
+
+    [<Fact>]
+    let doubleNeg () = checkParseSnapshot "--6"
+
+    [<Fact>]
+    let plusMulMixed () = checkParseSnapshot "2 + 3 * 4"
+
+    [<Fact>]
+    let letWithoutIn () = checkParseSnapshot "let x = 5"
+
+    [<Fact>]
+    let letWithoutIn2 () =
+        let source =
+            @"let x = 5
+let y = 6"
+
+        checkParseSnapshot source
+
+    [<Fact>]
+    let simpleAbs () = checkParseSnapshot @"\n. n"
+
+    [<Fact>]
+    let multiArgAbs () =
+        checkParseSnapshot @"\a b c. b*b - 4*a*c"
+        
+    [<Fact>]
+    let absAndLet () =
+        checkParseSnapshot @"let f = \x. x + 2 in f 2"
+
+    [<Fact>]
+    let letInArithmetic () =
+        let source = @"let inc x = x + 1 in inc 3"
+        checkParseSnapshot source
+
+    [<Fact>]
+    let digitsInIdents () = checkParseSnapshot "let x2 = 42 in x2"
+
+
+    [<Fact>]
+    let fib () =
+        checkParseSnapshot @"let rec fib n = if n < 2 then 1 else fib (n-1) + fib (n-2) in fib 10"
